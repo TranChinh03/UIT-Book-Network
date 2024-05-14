41ad9772d6d3d4cfc9243204e0472bdc2a0c1a73
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-14 14:10:22 +0700

    Pagination

--- uit-book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ uit-book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -11,7 +11,7 @@ import {PageResponseBookResponse} from "../../../../services/models/page-respons
 export class BookListComponent implements OnInit{
   bookResponse: PageResponseBookResponse = {}
   page = 0;
-  size = 5;
+  size = 4;
 
   constructor(
     private bookService: BookService,
@@ -32,5 +32,34 @@ export class BookListComponent implements OnInit{
         this.bookResponse = books;
       }
     })
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.bookResponse.totalPages as number - 1;
   }
 }
